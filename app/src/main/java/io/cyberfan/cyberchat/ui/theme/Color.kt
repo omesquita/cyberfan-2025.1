e829dc3a5a4ac6ec01bc6ded4eafa559624d1c15
name <email>
2025-05-26 20:29:36 -0300

    initial commit

---
+++ app/src/main/java/io/cyberfan/cyberchat/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package io.cyberfan.cyberchat.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val PrimaryBlue = Color(0xFF6366F1)
+val PrimaryPurple = Color(0xFF8B5CF6)
+val PrimaryPink = Color(0xFFEC4899)
+val SurfaceGray = Color(0xFFF5F5F5)
+val TextPrimary = Color(0xFF1F2937)
+val TextSecondary = Color(0xFF6B7280)
+val TextPlaceholder = Color(0xFF9CA3AF)
+val SurfaceVariant = Color(0xFFF9FAFB)
+val OutlineGray = Color(0xFFE5E7EB)
+val ChatBackground = Color(0xFFECE5DD)
